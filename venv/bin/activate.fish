2064c4cf8108b804eace292518c79303eb4d5699
ankitagrawal10 <jaiankitagrawal@gmail.com>
2025-03-25 11:03:31 +0530

    update something

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ankit/workflows/venv
+set -gx VIRTUAL_ENV /home/ankit/git_action/test-1/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
